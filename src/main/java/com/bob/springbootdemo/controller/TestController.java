ed6346f2e4b440f4f533d63fb915577241a9c3fe
bobshui <bobshui@163.com>
2020-12-06 11:44:21 +0800

    init

---
+++ src/main/java/com/bob/springbootdemo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.bob.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("")
+    public String selectAll(){
+        return "test";
+    }
+}
